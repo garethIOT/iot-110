ee485ca6ff88e6da858c39c8ff5218d27c3f8661
garethIOT <gareth.beale@boeing.com>
2017-01-30 15:32:24 +0000

    add SSE code

--- Labs/Lab3/webapp/main.py
+++ Labs/Lab3/webapp/main.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python
+import time
 from gpio import PiGpio
+from debouncer import Debouncer
 from flask import *
 
 app = Flask(__name__)
 pi_gpio = PiGpio()
+db = Debouncer()
 
 @app.route("/")
 def index():
@@ -55,8 +59,25 @@ def ledcommand():
 
     return "Led State Command:" + state + " for LED number:"+ str(led) + "\n"
     # -----------------------------------------------------------------------------
+# =========================== Endpoint: /myData ===============================
+# read the gpio states by GET method from curl for example
+# curl http://iot8e3c:5000/myData
+# -----------------------------------------------------------------------------
+@app.route('/myData')
+def myData():
+    def get_state_values():
+        while True:
+            # return the yield results on each loop, but never exits while loop
+            raw_switch = pi_gpio.read_switch()
+            debounced_switch = str(db.debounce(raw_switch))
+            led_red = str(pi_gpio.get_led(1))
+            led_grn = str(pi_gpio.get_led(2))
+            led_blu = str(pi_gpio.get_led(3))
+            yield('data: {0} {1} {2} {3}\n\n'.format(debounced_switch,led_red,led_grn,led_blu))
+            time.sleep(0.1)
+    return Response(get_state_values(), mimetype='text/event-stream')
+    # -----------------------------------------------------------------------------
 # ============================== API Routes ===================================
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-
+    app.run(host='0.0.0.0', debug=True, threaded=True)
