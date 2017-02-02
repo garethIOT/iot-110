7a81c9c03d74d55637a25b51fb4cf796983c3bff
garethIOT <gareth.beale@boeing.com>
2017-02-02 05:07:43 +0000

    implement button and LED state changes

--- Labs/Lab3/webapp/static/js/lab3.js
+++ Labs/Lab3/webapp/static/js/lab3.js
@@ -42,10 +42,12 @@
 
         /* update the Switch based on its SSE state monitor */
         function updateSwitch(switchValue) {
+	  var sw_up = '\u21e7';
+	  var sw_dn = '\u21e9';
           if (switchValue === '1') {
-            $('#switch').text('ON');
+            $('#switch').text(sw_dn);
           } else if (switchValue === '0') {
-            $('#switch').text('OFF');
+            $('#switch').text(sw_up);
           }
         }
 
@@ -53,28 +55,40 @@
         function updateLeds(ledNum,ledValue) {
           if (ledNum === 1) {
             if (ledValue === '1') {
-              $('#red_led_label').text('ON');
+             //$('#red_led_label').text('ON');
+              $('#red_led_label').toggleClass('label-default',false);
+              $('#red_led_label').toggleClass('label-danger',true);
               led1 = "ON"
             } else if (ledValue === '0') {
-              $('#red_led_label').text('OFF');
+              //$('#red_led_label').text('OFF');
+              $('#red_led_label').toggleClass('label-default',true);
+              $('#red_led_label').toggleClass('label-danger',false);
               led1 = "OFF"
             }
           }
           else if (ledNum === 2) {
             if (ledValue === '1') {
-              $('#grn_led_label').text('ON');
+              //$('#grn_led_label').text('ON');
+              $('#grn_led_label').toggleClass('label-default',false);
+              $('#grn_led_label').toggleClass('label-success',true);
               led2 = "ON"
             } else if (ledValue === '0') {
-              $('#grn_led_label').text('OFF');
+              //$('#grn_led_label').text('OFF');
+              $('#grn_led_label').toggleClass('label-default',true);
+              $('#grn_led_label').toggleClass('label-success',false);
               led2 = "OFF"
             }
           }
           else if (ledNum === 3) {
             if (ledValue === '1') {
-              $('#blu_led_label').text('ON');
+              //$('#blu_led_label').text('ON');
+              $('#blu_led_label').toggleClass('label-default',false);
+              $('#blu_led_label').toggleClass('label-primary',true);
               led3 = "ON"
             } else if (ledValue === '0') {
-              $('#blu_led_label').text('OFF');
+              //$('#blu_led_label').text('OFF');
+              $('#blu_led_label').toggleClass('label-default',true);
+              $('#blu_led_label').toggleClass('label-primary',false);
               led3 = "OFF"
             }
           }
